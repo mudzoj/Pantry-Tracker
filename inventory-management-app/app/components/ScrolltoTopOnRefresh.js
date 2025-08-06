c7b55925138c534a16c2011b4febca8b06a4442b
mudzoj <jalal.mud@gmail.com>
2025-08-05 18:37:30 -0600

    fixed imports

--- inventory-management-app/app/components/ScrolltoTopOnRefresh.js
+++ inventory-management-app/app/components/ScrolltoTopOnRefresh.js
@@ -3,9 +3,9 @@ import { useEffect } from "react";
 const ScrollToTopOnRefresh = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []); // Runs once on mount
+  }, []); 
 
-  return null; // This component does not render anything
+  return null; 
 };
 
 export default ScrollToTopOnRefresh;
